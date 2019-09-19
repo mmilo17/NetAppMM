66848e094923885b290dd9311348f02d966e7ac6
mmilo17 <majewskimilo2@gmail.com>
2019-09-19 17:16:16 +0100

    end of secion 4

--- NetApp.API/Controllers/ValuesController.cs
+++ NetApp.API/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace NetApp.API.Controllers
             _context = context;
         }
 
-        // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
