db7e5b741d25ca93f8e3d24441dfdf142132f197
mmilo17 <44226992+mmilo17@users.noreply.github.com>
2019-12-15 16:56:05 +0000

    Part12 half

--- NetApp.API/Dtos/UserForRegisterDto.cs
+++ NetApp.API/Dtos/UserForRegisterDto.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace NetApp.API.Dtos
@@ -8,7 +9,27 @@ namespace NetApp.API.Dtos
         public string Username {get; set; }
         
         [Required]
-        [StringLength(16, MinimumLength = 4, ErrorMessage = "You must specify password between 4- 16 characters")]
+        [StringLength(16, MinimumLength = 4, ErrorMessage = "You must specify password between 4 - 16 characters")]
         public string Password {get; set; }
+        [Required]
+        public string Gender {get; set; }
+        [Required]
+        public string Name {get; set; }
+        [Required]
+        public string Surname {get; set; }
+        [Required]
+        public DateTime DateOfBirth {get; set; }
+        [Required]
+        public string City {get; set; }
+        [Required]
+        public string Country {get; set; }
+        public DateTime Created {get; set; }
+        public DateTime LastActive {get; set; }
+
+        public UserForRegisterDto()
+        {
+            Created = DateTime.Now;
+            LastActive = DateTime.Now;
+        }
     }
 }
